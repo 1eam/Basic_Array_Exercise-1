7432a12cd1d79c628dceafcb0b7b225864afdccc
Esther <esther191299@gmail.com>
2019-09-15 19:21:42 +0200

    First commit

---
+++ src/arrayExercise/Main.java
@@ -0,0 +1,19 @@
+package arrayExercise;	//result = printed selected array
+
+public class Main {
+
+	public static void main(String[] args) {
+		String [] fruits = new String [8];
+		fruits [0] = "Apple";
+		fruits [1] = "Banana";
+		fruits [2] = "Pear";
+		fruits [3] = "Mandarine";
+		fruits [4] = "Orange";
+		fruits [5] = "Plumbs";
+		fruits [6] = "Melon";
+		fruits [7] = "Grapes";
+		
+		System.out.println (fruits [1]);
+	}
+	
+}
